64cbc0841387142edb36d8db77ffaae82380bb64
Steven Zapata <szapata427@gmail.com>
2018-09-09 20:36:29 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,12 +7,11 @@ suite_e: "Crumpet the Elf"
 }
 
 def select_winner(passengers)
-  winner = ""
+ winner = ""
 passengers.each do |suite, name|
   if suite == :suite_a && name.start_with?("A")
     winner = name
   end
-
 end
  
-winner  
+winner 
